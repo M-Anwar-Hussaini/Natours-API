c4bb704a60317129ffd0f9c89d30e2068ee7d0cf
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-04 17:15:00 +0430

    Implement the update controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -52,13 +52,29 @@ export const createTour = async (req, res) => {
   }
 };
 
-export const updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
+export const updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body },
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 export const deleteTour = (req, res) => {
