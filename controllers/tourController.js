4e7af9dbd9c4f302d55f4c5c4d73ed15e17af622
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-04 17:22:01 +0430

    Implement the delete action in the tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -77,9 +77,17 @@ export const updateTour = async (req, res) => {
   }
 };
 
-export const deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+export const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      message: 'The tour was successfully deleted',
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
