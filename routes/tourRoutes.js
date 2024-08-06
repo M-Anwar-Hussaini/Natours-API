68c4677d8f6539dd7228aa49081c97fba75bb245
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-06 19:18:03 +0430

    Create a new route for tour stats

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,11 +6,14 @@ import {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
 } from '../controllers/tourController.js';
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
 
 router.route('/').get(getAllTours).post(createTour);
 
