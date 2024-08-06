08b66ac0bf1f3d20d5f0dd8850918738a0474788
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-06 20:18:00 +0430

    Add monthly plan route to the tourRoutes.

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ import {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } from '../controllers/tourController.js';
 
 const router = express.Router();
@@ -14,6 +15,8 @@ const router = express.Router();
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/tour-stats').get(getTourStats);
+
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(createTour);
 
